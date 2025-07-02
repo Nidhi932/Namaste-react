3ffbd9f31c365ca6b9b973347f4789d7d29c0cf8
Nidhi932 <pandeynidhi9322@gmail.com>
2025-07-03 01:19:24 +0530

    ep1

---
+++ ep1/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "H1Tag!")
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
